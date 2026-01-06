c81b90449944c9beb0bbfd9564c243face2eb604
Vivek Lokhande <lvivek260@gmail.com>
2026-01-06 22:09:36 +0530

    First Commit

---
+++ Stock-Market/Stock-Market/Stock_MarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stock_MarketApp.swift
+//  Stock-Market
+//
+//  Created by Vivek Lokhande on 06/01/26.
+//
+
+import SwiftUI
+
+@main
+struct Stock_MarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
